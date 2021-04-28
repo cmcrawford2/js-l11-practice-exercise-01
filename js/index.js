4753f629badbeac9053243cf900a56cf84165d80
cmcrawford2 <crawford.cris@gmail.com>
2021-04-28 13:56:22 -0400

    Enabled drop-down menu

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector(".num-users");
 
-const getData = async function () {
-  const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+  const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
   const data = await usersRequest.json();
   console.log(data);
   const usersResult = data.results;
@@ -9,7 +10,7 @@ const getData = async function () {
   displayUsers(usersResult);
 }
 
-getData();
+getData(1);
 
 const displayUsers = function(userResults) {
   randomFolks.innerHTML = "";
@@ -26,3 +27,9 @@ const displayUsers = function(userResults) {
     randomFolks.append(userDiv);
   }
 }
+
+selectUserNumber.addEventListener("change", function (e) {
+  const numUsers = e.target.value;
+  console.log(numUsers);
+  getData(numUsers);
+})
